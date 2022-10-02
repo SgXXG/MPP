b9d39d5cef003ff64db96166871f63a0d1fcbb8c
Stanislav <72697394+SgXXG@users.noreply.github.com>
2022-10-02 13:00:20 +0300

    lab2

---
+++ Lab2/MPP_Lab2_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab2_1
+{
+    class Mutex
+    {
+        private int _holderId = -1;
+
+        public void Lock()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            while (Interlocked.CompareExchange(ref _holderId, id, -1) != -1)
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        public void Unlock()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            Interlocked.CompareExchange(ref _holderId, -1, id);
+        }
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var mutex = new Mutex();
+            for (var i = 0; i < 10; i++)
+            {
+                new Thread(() =>
+                {
+                    mutex.Lock();
+                    Console.WriteLine("Thread #" + Thread.CurrentThread.ManagedThreadId + " locked mutex.");
+                    Thread.Sleep(400);
+                    Console.WriteLine("Thread #" + Thread.CurrentThread.ManagedThreadId + " unlocked mutex.");
+                    mutex.Unlock();
+                }).Start();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab2/MPP_Lab2_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Мусор
+{
+    class OsHandle : IDisposable
+    {
+        [DllImport("Kernel32.dll")]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        private bool _disposed = false;
+        public IntPtr Handle { get; set; }
+
+        public OsHandle()
+        {
+            Handle = IntPtr.Zero;
+        }
+
+        ~OsHandle()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Close()
+        {
+            Dispose();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            lock (this)
+            {
+                if (!_disposed && Handle != IntPtr.Zero)
+                {
+                    CloseHandle(Handle);
+                    Handle = IntPtr.Zero;
+                }
+                _disposed = true;
+            }
+        }
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
