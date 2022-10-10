86e1aaa43d2afc67a564a3f28708769f899199de
Stanislav <72697394+SgXXG@users.noreply.github.com>
2022-10-10 18:45:35 +0300

    lab3 task1

---
+++ Lab3/MPP_Lab3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+
+namespace MPP_Lab3_1
+{
+    static class Program
+    {
+        static void ListTypesInAssembly(Assembly assembly)
+        {
+            var types = assembly.GetTypes().Where(t => t.IsPublic).OrderBy(t => t.Namespace + t.Name);
+            foreach (var type in types)
+            {
+                Console.WriteLine(type.FullName);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.LoadFrom("D:\\!Studing\\3 Course\\5 Sem\\СПП\\MPP_Lab1\\MPP_Lab1\\bin\\Debug\\net6.0\\MPP_Lab1_1.dll");
+            ListTypesInAssembly(assembly);
+            Console.ReadKey();
+        }
+    }
+}
